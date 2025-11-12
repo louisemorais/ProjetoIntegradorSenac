ada02ae4302230496158eae89b4ee3bd944a870d
Anne Daniela <annedanielasv@gmail.com>
2025-11-12 16:48:47 -0300

    feat: adiciona estado e estilização do componente calendário

--- agendaFacil/src/components/calendar/Calendario.jsx
+++ agendaFacil/src/components/calendar/Calendario.jsx
@@ -1,11 +1,30 @@
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import * as React from 'react'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar'
+import dayjs from 'dayjs'
 
 export default function Calendario() {
+    const [selectedDate, setSelectedDate] = React.useState(dayjs())
+
+    // atualiza a data quando o usuário selecionar uma nova
+    const handleDateChange = (newDate) => {
+        setSelectedDate(newDate)
+    }
+
     return (
         <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DateCalendar />
+            <DateCalendar
+                value={selectedDate}
+                onChange={handleDateChange}
+                sx={{
+                    borderRadius: 2,
+                    boxShadow: '0px 2px 8px rgba(0,0,0,0.08)',
+                    p: 1,
+                    width: 350,
+                    margin: '0 auto',
+                }}
+            />
         </LocalizationProvider>
     )
 }
