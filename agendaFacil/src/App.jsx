192a7c6c80959a01392b00fb75bb27358991abf9
wstvns <wstevandev@gmail.com>
2025-11-14 09:55:28 -0300

    feat: Configurar rotas da aplicação (React Router) - Rotas de autenticação: /login e /registro. - Rotas de prestador: /prestador/:id (Detalhes) e /agendamento/:prestadorId/:servicoId. - Rotas de usuário: /meus-agendamentos e /perfil. - Atualiza a rota base de /servicos para suportar /servicos e /servicos/:categoria

--- agendaFacil/src/App.jsx
+++ agendaFacil/src/App.jsx
@@ -1,6 +1,12 @@
 import {Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import Servicos from './pages/Servicos'
+import Login from './pages/Login'
+import Registro from './pages/Registro'
+import DetalhesPrestador from './pages/DetalhesPrestador'
+import Agendamento from './pages/Agendamento'
+import MeusAgendamentos from './pages/MeusAgendamentos'
+import Perfil from './pages/Perfil'
 import NavBar from './components/navbar/NavBar'
 import './App.css'
 
@@ -8,10 +14,17 @@ function App() {
   return (
     <>
       <NavBar/>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/servicos/:categoria" element={<Servicos />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/servicos" element={<Servicos />} />        
+          <Route path="/servicos/:categoria" element={<Servicos />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/registro" element={<Registro />} />
+          <Route path="/prestador/:id" element={<DetalhesPrestador />} />
+          <Route path="/agendamento/:prestadorId/:servicoId" element={<Agendamento />} />
+          <Route path="/meus-agendamentos" element={<MeusAgendamentos />} />
+          <Route path="/perfil" element={<Perfil />} />
+        </Routes>
     </>
   )
 }
